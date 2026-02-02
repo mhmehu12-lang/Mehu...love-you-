eec83bdcd19cc3d7bbc4da4ff4d05e4b49524289
mhmehu12-lang <mhmehu12@gmail.com>
2026-02-02 18:26:04 +0600

    Update mygirl.js

--- Script/commands/mygirl.js
+++ Script/commands/mygirl.js
@@ -12,39 +12,33 @@ module.exports.config = {
   role: 0,
   author: "MahMUD",
   category: "fun",
-  cooldowns: 5
+  cooldown: 5
 };
 
-module.exports.onStart = async ({ event, api, args }) => {
-  const obfuscatedAuthor = String.fromCharCode(77, 97, 104, 77, 85, 68); 
-    if (module.exports.config.author !== obfuscatedAuthor) {
-      return api.sendMessage("You are not authorized to change the author name.", event.threadID, event.messageID);
-    }
+module.exports.run = async ({ api, event, args }) => {
   try {
     const { threadID, messageID, senderID } = event;
-    const mention = Object.keys(event.mentions)[0] || (event.messageReply && event.messageReply.senderID);
+    const mention = Object.keys(event.mentions)[0] || 
+      (event.messageReply && event.messageReply.senderID);
 
     if (!mention)
       return api.sendMessage("Please tag or reply to 1 person", threadID, messageID);
 
-    const user1 = senderID;
-    const user2 = mention;
-
     const baseUrl = await baseApiUrl();
-    const apiUrl = `${baseUrl}/api/myboy?user1=${user1}&user2=${user2}`;
+    const apiUrl = `${baseUrl}/api/myboy?user1=${senderID}&user2=${mention}`;
 
     const response = await axios.get(apiUrl, { responseType: "arraybuffer" });
 
-    const imgPath = __dirname + `/cache/mygirl_${user1}_${user2}.png`;
-    fs.writeFileSync(imgPath, Buffer.from(response.data, "binary"));
+    const imgPath = __dirname + `/cache/mygirl_${senderID}_${mention}.png`;
+    fs.writeFileSync(imgPath, Buffer.from(response.data));
 
     api.sendMessage({
       body: `𝐓𝐇𝐀𝐓'𝐒 𝐌𝐀𝐇 𝐆𝐈𝐑𝐋 🖤`,
       attachment: fs.createReadStream(imgPath)
     }, threadID, () => fs.unlinkSync(imgPath), messageID);
 
-  } catch (error) {
-    console.error(error);
-    api.sendMessage("🥹error, contact MahMUD.", event.threadID, event.messageID);
+  } catch (err) {
+    console.error(err);
+    api.sendMessage("🥹 Error occurred.", event.threadID, event.messageID);
   }
 };
